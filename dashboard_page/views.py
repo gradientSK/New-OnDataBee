e1f5c2275000c3c8c672de0d4e47fb4f431e0fcc
Your Name <you@example.com>
2023-10-07 13:33:51 +0330

    dashboard page creation

--- dashboard_page/views.py
+++ dashboard_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard_manager (request):
-    return render(request, 'dashboard_page/dashboard_manager.html')gt
+    return render(request, 'dashboard_page/dashboard_manager.html')
